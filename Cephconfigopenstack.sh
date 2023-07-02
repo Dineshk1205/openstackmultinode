276bd0c31f0a2321a023361675d6205c036c1c60
Dineshk1205 <93421112+Dineshk1205@users.noreply.github.com>
2023-07-02 19:58:16 +0530

    Run on Ceph Node - Openstack pools&keys configuration

---
+++ Cephconfigopenstack.sh
@@ -0,0 +1,36 @@
+DeploymentIP= "172.90.0.33"
+ceph osd pool create volumes
+ceph osd pool create images
+ceph osd pool create backups
+ceph osd pool create vms
+rbd pool init volumes
+rbd pool init images
+rbd pool init backups
+rbd pool init vms
+
+ceph auth get-or-create client.glance mon 'profile rbd' osd 'profile rbd pool=images' mgr 'profile rbd pool=images'
+
+ceph auth get-or-create client.cinder mon 'profile rbd' osd 'profile rbd pool=volumes, profile rbd pool=vms, profile rbd-read-only pool=images' mgr 'profile rbd pool=volumes, profile rbd pool=vms'
+
+ceph auth get-or-create client.cinder-backup mon 'profile rbd' osd 'profile rbd pool=backups' mgr 'profile rbd pool=backups'
+
+ssh-copy-id root@$DeploymentIP
+scp -r /etc/ceph/ceph.conf root@$DeploymentIP:/etc/kolla/config/cinder/cinder-backup/ceph.conf
+scp -r /etc/ceph/ceph.conf root@$DeploymentIP:/etc/kolla/config/cinder/cinder-volume/ceph.conf
+scp -r /etc/ceph/ceph.conf root@$DeploymentIP:/etc/kolla/config/glance/ceph.conf
+scp -r /etc/ceph/ceph.conf root@$DeploymentIP:/etc/kolla/config/nova/ceph.conf
+
+ceph auth get-or-create client.cinder-backup  > /etc/ceph/ceph.client.cinder-backup.keyring
+ceph auth get-or-create client.cinder > /etc/ceph/ceph.client.cinder.keyring
+ceph auth get-or-create client.glance > /etc/ceph/ceph.client.glance.keyring
+
+
+
+scp -r /etc/ceph/ceph.client.glance.keyring root@$DeploymentIP:/etc/kolla/config/glance/ceph.client.glance.keyring
+scp -r /etc/ceph/ceph.client.cinder-backup.keyring  root@$DeploymentIP:/etc/kolla/config/cinder/cinder-backup/ceph.client.cinder-backup.keyring 
+scp -r /etc/ceph/ceph.client.cinder.keyring  root@$DeploymentIP:/etc/kolla/config/cinder/cinder-backup/ceph.client.cinder.keyring
+scp -r /etc/ceph/ceph.client.cinder.keyring  root@$DeploymentIP:/etc/kolla/config/cinder/cinder-volume/ceph.client.cinder.keyring
+scp -r /etc/ceph/ceph.client.cinder.keyring  root@$DeploymentIP:/etc/kolla/config/nova/ceph.client.cinder.keyring
+
+
+
